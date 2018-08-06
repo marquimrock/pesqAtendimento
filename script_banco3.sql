0522956acb71a08ba1b3a06b6e91b08bb0cc188d
marquimrock <marquim.ti@gmail.com>
2018-08-06 20:30:50 -0300

    adicao do banco do gestao e scrip de atualizacao do banco

--- script_banco3.sql
+++ script_banco3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 03-Ago-2018 às 02:58
+-- Generation Time: 07-Ago-2018 às 01:29
 -- Versão do servidor: 10.1.30-MariaDB
 -- PHP Version: 5.6.33
 
@@ -35,23 +35,25 @@ CREATE TABLE `pesquisa` (
   `questao1` text NOT NULL,
   `questao2` text NOT NULL,
   `questao3` text NOT NULL,
-  `data` datetime NOT NULL
+  `data` date NOT NULL,
+  `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `pesquisa`
 --
 
-INSERT INTO `pesquisa` (`id`, `comanda`, `vendedor`, `questao1`, `questao2`, `questao3`, `data`) VALUES
-(17, 9898, 'vendedor 1', 'bom', 'otimo', '', '0000-00-00 00:00:00'),
-(21, 990099, 'vendedor 1', 'bom', 'otimo', '', '0000-00-00 00:00:00'),
-(22, 0, 'vendedor 1', 'otimo', 'otimo', '', '0000-00-00 00:00:00'),
-(23, 44444444, 'vendedor 1', 'bom', 'otimo', '', '0000-00-00 00:00:00'),
-(24, 8888, 'vendedor 1', 'bom', 'otimo', '', '0000-00-00 00:00:00'),
-(25, 99999999, 'vendedor 1', 'bom', 'ruim', '', '0000-00-00 00:00:00'),
-(26, 666, 'vendedor 1', 'bom', 'bom', '', '0000-00-00 00:00:00'),
-(27, 99999, 'vendedor 1', 'bom', 'otimo', '', '0000-00-00 00:00:00'),
-(28, 77, 'vendedor 1', 'bom', 'otimo', '', '0000-00-00 00:00:00');
+INSERT INTO `pesquisa` (`id`, `comanda`, `vendedor`, `questao1`, `questao2`, `questao3`, `data`, `hora`) VALUES
+(58, 1, 'BRUNA', 'bom', 'bom', 'bom', '2018-08-04', '00:00:00'),
+(59, 2, 'CAMILA', 'bom', 'bom', 'bom', '2018-08-04', '00:00:00'),
+(60, 2, 'CAMILA', 'otimo', 'otimo', 'otimo', '2018-08-04', '00:00:00'),
+(61, 1, 'BRUNA', 'bom', 'bom', 'bom', '2018-08-04', '00:00:00'),
+(62, 55, '', 'ruim', 'bom', 'otimo', '2018-08-04', '00:00:00'),
+(63, 1, 'BRUNA', 'ruim', 'ruim', 'ruim', '2018-08-04', '00:00:00'),
+(64, 2, 'CAMILA', 'bom', 'bom', 'bom', '2018-08-05', '00:00:00'),
+(65, 1, 'BRUNA', 'otimo', 'otimo', 'otimo', '2018-08-06', '00:00:00'),
+(66, 1, 'BRUNA', 'ruim', 'ruim', 'ruim', '2018-08-06', '19:58:00'),
+(67, 2, 'CAMILA', 'ruim', 'ruim', 'ruim', '2018-08-06', '19:59:00');
 
 --
 -- Indexes for dumped tables
@@ -73,7 +75,7 @@ ALTER TABLE `pesquisa`
 -- AUTO_INCREMENT for table `pesquisa`
 --
 ALTER TABLE `pesquisa`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
