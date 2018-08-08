7d7b6442ed8f25073195e412e99af1edbf1197c6
marquimrock <marquim.ti@gmail.com>
2018-08-07 23:36:31 -0300

    validacao da comanda com JS

--- index.php
+++ index.php
@@ -8,13 +8,30 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Pesquisa de Satisfação do Cliente</title>
         <link rel="stylesheet" href="assets/css/bootstrap.css"/>
+        <script>
+            function somenteNumeros(num) {
+                var er = /[^0-9.]/;
+                er.lastIndex = 0;
+                var campo = num;
+                if (er.test(campo.value)) {
+                    campo.value = "";
+                }
+            }
+            function valida_form() {
+                if (document.getElementById("comanda").value.length < 8) {
+                    alert('Por favor, digite o numero da comanda com 8 dígitos!');
+                    document.getElementById("comanda").focus();
+                    return false
+                }
+            }
+        </script>
     </head>
     <body>
         <div class="container-fluid" style="background-color: #FFFFCC;" align="center">
             <img src="logo.png" width="255" height="187">
-            <form id="frmQuestao1" class="form-inline" name="frmQuestao1" method="post" action="pagina2.php">
+            <form id="frmQuestao1" class="form-inline" name="frmQuestao1" method="post" action="pagina2.php" onsubmit="return valida_form(this)">
                 <label name="lblComanda"><h2><b>Comanda</b></h2></label><br>
-                <input type="text" class="form-control" name="comanda" id="comanda"><br><br>
+                <input type="text" class="form-control" name="comanda" id="comanda" onkeyup="somenteNumeros(this);" maxlength="8    "><br><br>
                 <label name="lblQuestao1"><h4>1/3. Como você avalia o tempo de espera do atendimento?</h4></label><br>
                 <button name="btnQuestao1" type="submit" class="btn btn-danger" value="ruim"><b>Ruim!</b></button>
                 <button name="btnQuestao1" type="submit" class="btn btn-warning" value="bom"><b>Bom!</b></button>
