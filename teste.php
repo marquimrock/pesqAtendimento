b94122fe1f8bc9306e3923a9215ab01bacf27ef0
leandrolac <leandro.alvesdecubas@gmail.com>
2018-08-08 21:08:30 -0300

    resolvendo conflito em teste

--- teste.php
+++ teste.php
@@ -1,9 +1,6 @@
 <?php
-include './pesquisa.php';
+$servidor = '127.0.0.1:C:/Mobility_Gestao/banco/banco.FDB';
 
-$pesquisa = new pesquisa();
-$pesquisa->setQuestao1(1);
-$pesquisa->setQuestao2(1);
-$pesquisa->setQuestao2(1);
-
-filtraPesquisa($pesquisa);
+//conexão com o banco, se der erro mostrara uma mensagem.
+if (!($dbh = ibase_connect($servidor, 'SYSDBA', 'masterkey')))
+    die('Erro ao conectar: ' . ibase_errmsg());
