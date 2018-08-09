7ec42ceaf3b6b18053f761b63f6b1203efbf59bb
marquimrock <marquim.ti@gmail.com>
2018-08-08 23:53:18 -0300

    calendario teste

--- teste.php
+++ teste.php
@@ -1,10 +1,40 @@
-<?php
+<!doctype html>
 
-//ibase_connect — Abre uma conexão com um banco de dados InterBase
-//pode ser colocado o IP, ou nome do computador onde esta o banco de dados
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Calendário jQuery</title> 
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
+        <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+        <script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
+        <script>
+            $(function () {
+                $("#calendario").datepicker({
+                    dateFormat: 'dd/mm/yy',
+                    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'],
+                    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+                    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+                    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+                    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+                });
+            });
+        </script>
+        <style>
+            body{
+                background-color: red;
+            }
 
-$servidor = '127.0.0.1:C:/Mobility_Gestao/banco/banco.FDB';
-
-//conexão com o banco, se der erro mostrara uma mensagem.
-if (!($dbh = ibase_connect($servidor1, 'SYSDBA', 'masterkey')))
-    die('Erro ao conectar: ' . ibase_errmsg());
+        </style>
+    </head>
+    <body >
+        <form method="post" action="teste.php" name="calendario">
+              
+            <p>Data: <input type="text" id="calendario" name="calendario"/></p>
+        <?php 
+        echo $_POST['calendario'];
+        
+        ?>  
+        <input type="submit" value="gravar">
+        </form>
+    </body>
+</html>
